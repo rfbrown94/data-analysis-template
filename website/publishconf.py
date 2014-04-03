1c88f5a38ae065ae4841a87b24fa7ea0f5ddddf5
Oski <oski@ubuntu.(none)>
2014-04-03 10:58:30 -0700

    Modify Website Configuration Files

--- website/publishconf.py
+++ website/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://benoitdherin.github.io/data-analysis-template/'
+SITEURL = 'http://rfbrown94.github.io/Money-Squad-Data-Analysis'
 
 RELATIVE_URLS = False
 
